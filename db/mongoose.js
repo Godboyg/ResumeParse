bb96464db10b2092cec19bea2df6c63856acfe8b
kushal Singh Negi <89735938+Godboyg@users.noreply.github.com>
2025-02-22 09:42:41 +0530

    Update mongoose.js

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/neoBuild").then(()=>{
+mongoose.connect(process.env.DB_URL).then(()=>{
     console.log("db connected");
 });
 
